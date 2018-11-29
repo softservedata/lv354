a952982be88ea1ffe5ada7ef9d5b3307a101379d
bebeber <hahaha@gmail.com>
2018-11-29 20:19:41 +0200

    Examples, static, super, this, override, parameters

---
+++ src/main/java/com/softserve/train/ABase.java
@@ -0,0 +1,17 @@
+package com.softserve.train;
+
+public abstract class ABase {
+
+	public ABase() {
+		System.out.println("Constructor ABase()");
+	}
+	
+	static void m0() {
+		System.out.println("ABase: static void m0()");
+	}
+
+	void method0() {
+		System.out.println("ABase: static void method0()");
+	}
+
+}
---
+++ src/main/java/com/softserve/train/Appl.java
@@ -0,0 +1,74 @@
+package com.softserve.train;
+
+public class Appl {
+	
+//	public static void my(ClassB b) {
+//		System.out.println("my(ClassB b)");
+//	}
+//
+//	public static void my(ClassA a) {
+//		System.out.println("my(ClassA a)");
+//	}
+
+	public void my(Object b) {
+		System.out.println("my(Object b)");
+	}
+
+	public void my(ClassB b) {
+		System.out.println("my(ClassB b)");
+	}
+
+	public void my(ClassA a) {
+		System.out.println("my(ClassA a)");
+	}
+
+//	public void my(RuntimeException e) { // Chield
+//		System.out.println("my(RuntimeException e)");
+//	}
+
+//	public void my(Throwable e) {
+//		System.out.println("my(Throwable e)");
+//	}
+//
+//	public void my(Exception e) {
+//		System.out.println("my(Exception e)");
+//	}
+//
+//	public void my(RuntimeException e) { // Chield
+//		System.out.println("my(RuntimeException e)");
+//	}
+//
+//	public void my(CustomException e) { // Chield
+//		System.out.println("my(CustomException e)");
+//	}
+	
+	public static void main(String[] args) {
+		//ClassA a = new ClassA(121);
+		//a.method1();
+		//a.m0();
+		//ABase.m0();
+		//
+		//ClassB b = new ClassB();
+		//ClassA b = new ClassA(121);
+		//ABase b = new ClassA(121);
+		//b.method0();
+		//b.m0();
+		//ABase.m0();
+		//ClassA.m0();
+		//
+		//my(b); // Error for ABase b
+		//my(null); // my(ClassB b)
+		//
+		//Appl appl = new Appl();
+		//appl.my(1); // my(Object b)
+		//appl.my(null); // my(ClassB b) // my(RuntimeException e)
+		//appl.my(b); // my(Object b)
+		//
+//		ClassC c = new ClassC();
+//		ClassC.ClassD d = c.new ClassD();
+//		d.md();
+		//
+		ClassC.ClassD d = new ClassC.ClassD();
+		d.md();
+	}
+}
